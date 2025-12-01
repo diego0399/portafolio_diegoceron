b068e96da7a70260c8f7868a945dd921c6be54fe
diego0399 <86578035+diego0399@users.noreply.github.com>
2025-11-30 20:09:44 -0600

    Update Next.js config for image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  basePath: "/portafolio_diegoceron",
-  assetPrefix: "/portafolio_diegoceron",
   images: {
-    unoptimized: true,
+    unoptimized: false,
   },
-  trailingSlash: true,
 };
 
 export default nextConfig;
